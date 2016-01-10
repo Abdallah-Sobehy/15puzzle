2d8e2fa5656351f3d483bf72b349eea0df88388d
Abdallah Sobehy <a-sobehy@hotmail.com>
2016-01-11 00:20:55 +0100

    Added slide, shuffle, get_position functions to Board, and tested them in TestBoard

--- src/puzzle15/Board.java
+++ src/puzzle15/Board.java
@@ -1,4 +1,9 @@
 package puzzle15;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 /**
  * @author Abdalla Sobehy, Fred Aklamanu, Mohsin Kazmi, Renaud
  * @version 1
@@ -26,7 +31,13 @@ public class Board
 		// write the empty tile to -1 
 		board[dim-1][dim-1] = -1;
 	}
-	
+	/*
+	 * @return 2D representation of the board
+	 */
+	public int [][] get_board()
+	{
+		return board;
+	}
 	/*
 	 * Displays the board
 	 */
@@ -68,5 +79,110 @@ public class Board
 		}
 		System.out.println();
 	}
+	/*
+	 * Finds the index of a tile in the board
+	 * @param tile number on the tile to be searched or -1 to get the index of the empty tile.
+	 * @return the index of the searched tile.
+	 * @throws IllegalArgumentException if the searched tile is not from 1 to 15 or -1
+	 */
+	public int [] get_position(int tile) throws IllegalArgumentException
+	{
+		if ( (tile < 1 || tile > 15) && tile != -1 )
+			throw new IllegalArgumentException("Wrong inout for get_position function");
+		for ( int i = 0 ; i < dim ; i++)
+		{
+			for ( int j = 0 ; j < dim ; j++)
+			{
+				if (board[i][j] == tile)
+				{
+					int [] index = {i,j};
+					return index;
+				}
+			}
+		}
+		return null;
+	}
+	
+	/*
+	 * Slides a tile up/down/right/left depending on the place of the empty tile.
+	 * @param direction a string to specify the direction of sliding of the tile. 
+	 * should take one of the following values "up", "down", "right", "left"
+	 * @return true if the move is successfully executed
+	 * false if the move is not possible to be done e.g. sliding up when the empty tile is in the last row
+	 */
+	public boolean slide(String direction ) throws IllegalArgumentException
+	{
+		// Get index of the empty tile
+		int [] empty_i = get_position(-1);
+		// tile number to be slided
+		int moved_tile;
+		switch (direction)
+		{
+			case "up":
+				// Move is not possible if the empty tile is in the last row
+				if (empty_i[0] == dim - 1)
+					return false;
+				// The tile to be moved is the one under the empty tile
+				moved_tile = board[empty_i[0]+1][empty_i[1]];
+				// Switch moved tile with empty tile
+				board[empty_i[0]][empty_i[1]] = moved_tile;
+				board[empty_i[0]+1][empty_i[1]] = -1;
+				break;
+			case "down":
+				// Move is not possible if the empty tile is in the first row
+				if (empty_i[0] == 0)
+					return false;
+				// The tile to be moved is the one above the empty tile
+				moved_tile = board[empty_i[0]-1][empty_i[1]];
+				// Switch moved tile with empty tile
+				board[empty_i[0]][empty_i[1]] = moved_tile;
+				board[empty_i[0]-1][empty_i[1]] = -1;
+				break;
+			case "right":
+				// Move is not possible if the empty tile is in the first column
+				if (empty_i[1] == 0)
+					return false;
+				// The tile to be moved is the one to the left of the empty tile
+				moved_tile = board[empty_i[0]][empty_i[1]-1];
+				// Switch moved tile with empty tile
+				board[empty_i[0]][empty_i[1]] = moved_tile;
+				board[empty_i[0]][empty_i[1]-1] = -1;
+				break;
+			case "left":
+				// Move is not possible if the empty tile is in the last column
+				if (empty_i[1] == dim -1)
+					return false;
+				// The tile to be moved is the one to the right of the empty tile
+				moved_tile = board[empty_i[0]][empty_i[1]+1];
+				// Switch moved tile with empty tile
+				board[empty_i[0]][empty_i[1]] = moved_tile;
+				board[empty_i[0]][empty_i[1]+1] = -1;
+				break;
+			default:
+				throw new IllegalArgumentException("Wrong input to slide function");
+		}
+			return true;
+	}
 
+	/*
+	 * Shuffles the board with random number of moves.
+	 * @param num_moves number of moves to be randomly executed on the board.
+	 */
+	public void shuffle(int num_moves)
+	{
+		// Map to map randomly generated integers to directions of moves
+		Map map = new HashMap();
+		map.put(0,"up"); map.put(1, "down"); map.put(2,"right"); map.put(3, "left");
+		int rnd;
+		for (int i = 0 ; i < num_moves ; i++)
+		{
+			Random ran = new Random();
+			rnd = ran.nextInt(4);
+			System.out.println("Random move is : " + map.get(rnd));
+			// If the randomly selected move is not possible decrease i to repeat.
+			if (!slide((String) map.get(rnd)))
+				i--;
+			
+		}
+	}
 }
--- src/tests/TestBoard.java
+++ src/tests/TestBoard.java
@@ -10,5 +10,76 @@ public class TestBoard {
 		// Initialize board and display
 		Board b = new Board();
 		b.display();
+		// Slide up should return false
+		System.out.println("Attempt to slide up.");
+		if (b.slide("up"))
+		{
+			System.out.println("Unexpected outcome: slide up when empty tile in last row.");
+			b.display();
+		}
+		else System.out.println("can not slide up AS EXPECTED.");
+		// Slide left Should return false
+		System.out.println("Attempt to slide left.");
+		if (b.slide("left"))
+		{
+			System.out.println("Unexpected outcome: slide left when empty tile in last column.");
+			b.display();
+		}
+		else System.out.println("can not slide left AS EXPECTED.");
+		// Slide down should return true
+		System.out.println("Attempt to slide down.");
+		if (b.slide("down"))
+			b.display();
+		else System.out.println("Unexpected result: sliding down rejected when it should be accepted.");
+		// Slide right should return true
+		System.out.println("Attempt to slide right.");
+		if (b.slide("right"))
+			b.display();
+		else System.out.println("Unexpected result: sliding right rejected when it should be accepted.");
+		// Slide up
+		System.out.println("Attempt to slide up.");
+		if (b.slide("up"))
+			b.display();
+		else System.out.println("Unexpected result: sliding up rejected when it should be accepted.");
+		// Slide left
+		System.out.println("Attempt to slide left.");
+		if (b.slide("left"))
+			b.display();
+		else System.out.println("Unexpected result: sliding left rejected when it should be accepted.");
+		
+		System.out.println("Attempt to slide right then down 3 times.");
+		for (int i = 0 ; i<3 ;i++)
+		{
+			b.slide("right");
+			b.slide("down");
+		}
+		b.display();
+		
+		// Slide Right should return false
+		System.out.println("Attempt to slide Right.");
+		if (b.slide("right"))
+		{
+			System.out.println("Unexpected outcome: slide right when empty tile in first column.");
+			b.display();
+		}
+		else System.out.println("can not slide right AS EXPECTED.");
+		// Slide down Should return false
+		System.out.println("Attempt to slide down.");
+		if (b.slide("down"))
+		{
+			System.out.println("Unexpected outcome: slide down when empty tile in first row.");
+			b.display();
+		}
+		else System.out.println("can not slide down AS EXPECTED.");
+		
+		// Test shuffle function
+		System.out.println("Attempt to shuffle the board with 1 random move.");
+		b.shuffle(1);
+		b.display();
+		
+		// Test shuffle function
+		System.out.println("Attempt to shuffle the board with 3 random moves.");
+		b.shuffle(3);
+		b.display();
 	}
 }
